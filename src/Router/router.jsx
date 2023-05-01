6beb6450068d9103d86de206d6e28262a88207e6
abrahman002 <rahmanx661@gmail.com>
2023-05-01 22:13:27 +0600

    added router components

--- src/Router/router.jsx
+++ src/Router/router.jsx
@@ -2,12 +2,36 @@ import {
     createBrowserRouter,
   } from "react-router-dom";
 import Main from "../Layout/Main";
+import Home from "../components/Home/Home";
+import About from '../components/Page/Contact/Contact';
+import Contact from "../components/Page/Contact/Contact";
+import Blog from '../components/Page/Blog/Blog';
 
 
   const router = createBrowserRouter([
     {
       path: "/",
       element:<Main></Main>,
+      children:[
+        {
+            path:'/',
+            element:<Home></Home>
+        },
+        {
+            path:'/about',
+            element:<About></About>
+        }
+        ,
+        {
+            path:'/contact',
+            element:<Contact></Contact>
+        },
+        {
+            path:'/blog',
+            element:<Blog></Blog>
+        }
+
+      ]
     },
   ]);
   
